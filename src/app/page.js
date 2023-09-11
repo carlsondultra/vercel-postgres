daa2ff434ce391f62371c7b39c12157ffb63228e
carlsondultra <carlsondultra@users.noreply.github.com>
2023-09-11 17:45:57 -0400

    modifying landing page css

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,55 @@
 import Image from 'next/image'
+import Link from 'next/link'
 
 export default function Home() {
   return (
-    <div>Hello World</div>
+    <main className="relative flex min-h-screen flex-col items-center justify-center">
+      <h1 className="pt-4 pb-8 bg-gradient-to-br from-black via-[#171717] to-[#575757] bg-clip-text text-center text-4xl font-medium tracking-tight text-transparent md:text-7xl">
+        Vercel Postgres
+      </h1>
+      <p className="font-light text-gray-600 w-full max-w-lg text-center mt-6">
+        Built with{' '}
+        <Link
+          href="https://nextjs.org/docs"
+          className="font-medium underline underline-offset-4 hover:text-black transition-colors"
+        >
+          Next.js App Router
+        </Link>
+        .
+      </p>
+
+      <div className="flex justify-center space-x-5 pt-10 mt-10 border-t border-gray-300 w-full max-w-xl text-gray-600">
+        <Link
+          href="https://postgres-prisma.vercel.app/"
+          className="font-medium underline underline-offset-4 hover:text-black transition-colors"
+        >
+          Contact
+        </Link>
+        <Link
+          href="https://postgres-kysely.vercel.app/"
+          className="font-medium underline underline-offset-4 hover:text-black transition-colors"
+        >
+          About Us
+        </Link>
+      </div>
+
+      <div className="sm:absolute sm:bottom-0 w-full px-20 py-10 flex justify-between">
+        <Link href="https://vercel.com">
+          <Image
+            src="/vercel.svg"
+            alt="Vercel Logo"
+            width={100}
+            height={24}
+            priority
+          />
+        </Link>
+        <Link
+          href="https://github.com/vercel/examples/tree/main/storage/postgres-starter"
+          className="flex items-center space-x-2"
+        >
+          <p className="font-light">Testing</p>
+        </Link>
+      </div>
+    </main>
   )
 }
